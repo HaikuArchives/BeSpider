d7e2d7f27f32b2d6cbe1a3f2f31d4dafc5479a4c
Przemysław Buczkowski <przemub@przemub.pl>
2013-12-01 16:19:07 +0100

    moving cards

--- Source/SpiderView.h
+++ Source/SpiderView.h
@@ -17,7 +17,7 @@
 
 typedef struct card {
 	short fValue = -1;
-	bool fRevealed = true;	
+	bool fRevealed = false;	
 } card;
 
 
@@ -27,18 +27,29 @@ public:
 	
 	virtual void Draw(BRect rect);
 	virtual void MouseDown(BPoint point);
-								
+	virtual void MouseMoved(BPoint point,
+		uint32 transit, const BMessage* message);
+	virtual void MouseUp(BPoint point);
+
 private:
 	void _LoadBitmaps();
 	void _GenerateBoard();
-	short _FindFirstFree(short stock);
+	void _CheckBoard();
+	inline short _FindFirstFree(short stock);
 	
 	BBitmap* fSpades[13];
 	BBitmap* fBack;
 	
-	card fBoard[10][15];
+	card fBoard[10][25];
 	short fFreeCards[13];
 	short fStock;
+	
+	short fPickedCardBoardPos[1];
+	BPoint fPickedCardPos;
+	card fPickedCard;
+	bool fIsCardPicked;
+	bool fIsStackPicked;
+	short fLastPickedCardPos;
 };
 
 #endif // _SPIDERVIEW_H_
