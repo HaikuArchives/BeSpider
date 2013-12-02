409194d0071cc677ed56919ab53483d2a305284f
Przemysław Buczkowski <przemub@przemub.pl>
2013-12-02 15:59:50 +0100

    "about" text

--- Source/SpiderApplication.cpp
+++ Source/SpiderApplication.cpp
@@ -24,7 +24,7 @@ SpiderApplication::SpiderApplication()
 void SpiderApplication::AboutRequested()
 {
 	// TODO Replace with real about window
-	(new BAlert("AboutAlert", "BeSpider", "OK"))->Go();
+	(new BAlert("AboutAlert", "BeSpider 0.1\na spider solitaire clone for Haiku\n\nBeSpider (c) 2013 Przemysław Buczkowski <przemub@przemub.pl>\nDistributed under the terms of the MIT license.\n\nSVG-Z-cards (c) 2008 David Bellot, Brandon Ardiente\nLicensed under GNU Lesser General Public License 2.1.\n\nFanfare sound (c) 2009 Anthony Batiste\nLicensed under Creative Commons Attribution 3.0.", "OK"))->Go();
 }
 
 
